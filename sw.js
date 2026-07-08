f9288f64c71742ff8a36b6f5d5531c537623092c
komaki464-glitch <komaki464@gmail.com>
2026-07-08 16:49:04 +0900

    Add files via upload
    Add PWA files

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'pokesuri-counter-v11';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './apple-touch-icon.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
